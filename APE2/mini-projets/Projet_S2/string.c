f19878caac2fa6c51cdd90a7fbb319724b567e26
anpar <paris.antoine.paris@gmail.com>
2015-02-23 09:07:04 +0100

    Use malloc in tolowerstr

--- APE2/mini-projets/Projet_S2/string.c
+++ APE2/mini-projets/Projet_S2/string.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+// Avoid warning message 'implicit declaration of function...'
+#include <ctype.h>
+#include <string.h>
 
 size_t strlen(const char *s) {
 	int i = 0;
@@ -20,20 +23,22 @@ char *strcat(char *dest, const char *src) {
 }
 
 char *tolowerstr(const char *str) {
-	int i = 0;
-	char newstr[strlen(str)];
-	while(*(str+i) != '\0') {
-		newstr[i] = tolower(str[i]);
-		i++;
+	int i;
+	size_t len = strlen(str);
+	char *ptr = (char *) malloc(sizeof(char)*(len+1));
+	if (ptr != NULL) {
+		for (i = 0; i < len; i++) {
+			*(ptr+i) = tolower(*(str+i));
+		}
 	}
-	return(newstr);
+	return(ptr);
 }
 
 int strcasecmp(const char *s1, const char *s2) {
 	return(strcmp(tolowerstr(s1), tolowerstr(s2)));
 }
 
-void main (int argc, const char *argv[]) {
+int main (int argc, const char *argv[]) {
 	// Tests
 	char foo[] = "foo";
 	printf("The length of foo is %zu.\n", strlen(foo));
@@ -44,4 +49,6 @@ void main (int argc, const char *argv[]) {
 	const char str1[] = "Tintin";
 	const char str2[] = "tinTin";
 	printf("strcasecmp(str1,str2) returns %d.\n", strcasecmp(str1,str2));
+	return(EXIT_SUCCESS);
 }
+
