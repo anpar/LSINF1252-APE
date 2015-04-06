84498608eec5cb6945dcbc9042900cf47ab3758e
anpar <paris.antoine.paris@gmail.com>
2015-04-06 10:57:32 +0200

    Add max-args bash script

--- APE1/petits-programmes/args.c
+++ APE1/petits-programmes/args.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char *argv[]) {
-    printf("Number of arguments : %d.\n", argc);
+    // printf("Number of arguments : %d.\n", argc);
+    return(EXIT_SUCCESS);
 }
